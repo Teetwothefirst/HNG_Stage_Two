7520a5d7b5fdc20ce4b08c1917563b0a22016207
teetwothesecond <159890208+teetwothesecond@users.noreply.github.com>
2024-07-11 15:10:12 -0700

    Changes, added ProductDetails.jsx

---
+++ SkinSphere/src/ProductDetails.jsx
@@ -0,0 +1,13 @@
+function ProductDetails(){
+    
+    console.log("Check 1,2,3");
+    return(
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default ProductDetails;
